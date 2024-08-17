fcac1d98f6102084b5a969b821f2adc835c104a5
diaaALshalabi <diaawael13@icloud.com>
2024-08-17 21:09:43 +0300

    update

--- src/homePage/Mytestcases.java
+++ src/homePage/Mytestcases.java
@@ -1,17 +1,14 @@
 package homePage;
 
-import java.io.File;
-import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Random;
-
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -19,100 +16,125 @@ public class Mytestcases {
 
 	WebDriver driver = new ChromeDriver();
 	String WebSite = "https://global.almosafer.com/en";
-	JavascriptExecutor js = (JavascriptExecutor) driver;
 	Random rand = new Random();
+	String ExpectedDefaultLanage = "en";
 
 	@BeforeTest
 	public void MySetup() {
 		driver.manage().window().maximize();
 		driver.get(WebSite);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.findElement(By.cssSelector(".sc-jTzLTM.hQpNle.cta__button.cta__saudi.btn.btn-primary")).click();
 
 	}
 
-	@Test(enabled = false)
-	public void Check() throws IOException, InterruptedException {
+	@Test(priority = 1)
+	public void CheckTheDefaultLangugeIsEnglish() {
+		;
+		String ActualLanguage = driver.findElement(By.tagName("html")).getAttribute("lang");
 
-		WebElement saudibutton = driver
-				.findElement(By.cssSelector(".sc-jTzLTM.hQpNle.cta__button.cta__saudi.btn.btn-primary"));
-		saudibutton.click();
+		Assert.assertEquals(ActualLanguage, ExpectedDefaultLanage);
 
-		File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-		File destination = new File("screenshots/screenshot.png");
-		FileUtils.copyFile(screenshot, destination);
+	}
 
-		WebElement ContactUs = driver.findElement(By.cssSelector(".sc-hUfwpO.bWcsTG"));
-		ContactUs.click();
-		driver.navigate().back();
-		WebElement Logo = driver.findElement(By.cssSelector(".sc-bdVaJa.bxRSiR.sc-ekulBa.eYboXF"));
-		if (Logo.isDisplayed()) {
-			System.out.println("true");
-		} else {
-			System.out.println("false");
-		}
-		js.executeScript("window.scrollTo(0,8000)", Logo);
-		Thread.sleep(2000);
-		WebElement Hotelsbutton = driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
-		Hotelsbutton.click();
-		Thread.sleep(2000);
-		WebElement Flights = driver.findElement(By.cssSelector(
-				"body > div:nth-child(1) > section:nth-child(3) > div:nth-child(4) > div:nth-child(1) > div:nth-child(1) > nav:nth-child(1) > a:nth-child(1) > div:nth-child(1)"));
-		Flights.click();
-		Thread.sleep(2000);
-		js.executeScript("window.scrollTo(0,300)", Flights);
+	@Test(priority = 2)
+	public void CheckdefaultCurrency() {
+		String ExpectedCurrency = "SAR";
+		WebElement Currency = driver.findElement(By.xpath("//button[@data-testid='Header__CurrencySelector']"));
 
-		WebElement FlightDeparture = driver.findElement(By.cssSelector(".sc-iLVFha.sc-cgzHhG.TmOLL"));
-		FlightDeparture.click();
+		String ActualCurrency = Currency.getText();
 
-		File screenshot1 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-		File destination1 = new File("screenshots1/screenshot.png");
-		FileUtils.copyFile(screenshot1, destination1);
+		Assert.assertEquals(ActualCurrency, ExpectedCurrency);
+	}
+
+	@Test(priority = 3)
+	public void CheckContactNumber() {
+		String ExpectedContactNumber = "+966554400000";
+		String ActualContactNumber = driver.findElement(By.tagName("strong")).getText();
+
+		Assert.assertEquals(ActualContactNumber, ExpectedContactNumber);
+	}
+
+	@Test(priority = 4)
+	public void CheckQitagLogo() {
+		boolean ExpectedResultsForTheLogo = true;
+		WebElement theFooter = driver.findElement(By.tagName("footer"));
+
+		WebElement logo = theFooter.findElement(By.cssSelector(".sc-fihHvN.eYrDjb"))
+				.findElement(By.cssSelector(".sc-bdVaJa.bxRSiR.sc-ekulBa.eYboXF"));
+
+		boolean ActualResultForThelogo = logo.isDisplayed();
+
+		Assert.assertEquals(ActualResultForThelogo, ExpectedResultsForTheLogo);
+
+	}
+
+	@Test(priority = 5)
+	public void TestHotelTabIsNotSelected() {
+		String expectedValue = "false";
+		WebElement HotelTab = driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
+		String ActualValue = HotelTab.getAttribute("aria-selected");
+
+		Assert.assertEquals(ActualValue, expectedValue);
+
 	}
 
 	@Test
-	public void RandomLanguage() {
+	public void CheckDepatureDate() {
 
-		WebElement saudibutton = driver
-				.findElement(By.cssSelector(".sc-jTzLTM.hQpNle.cta__button.cta__saudi.btn.btn-primary"));
-		saudibutton.click();
+		LocalDate TodayDate = LocalDate.now();
 
-		int Randomlanguage = rand.nextInt(2);
+		int Today = TodayDate.getDayOfMonth();
+		int tomorrow = TodayDate.plusDays(1).getDayOfMonth();
+		int TheDayAfterTomorrow = TodayDate.plusDays(2).getDayOfMonth();
 
-		if (Randomlanguage == 0) {
-			WebElement englishOption = driver.findElement(By.cssSelector(".sc-gkFcWv.hcOfTd"));
-			englishOption.click();
-		
+		List<WebElement> departureAndArrival = driver.findElements(By.className("LiroG"));
+
+		String ActualDeparture = departureAndArrival.get(0).getText();
+		String ACTUALReturn = departureAndArrival.get(1).getText();
+
+		int ActualDepartureAsInt = Integer.parseInt(ActualDeparture);
+		int ActualReturndateASIn = Integer.parseInt(ACTUALReturn);
+
+		Assert.assertEquals(ActualDepartureAsInt, tomorrow);
+		Assert.assertEquals(ActualReturndateASIn, TheDayAfterTomorrow);
+
+	}
+
+	@Test(priority = 7)
+
+	public void RandomlyChangeTheLanguage() {
+		String[] URLS = { "https://www.almosafer.com/en", "https://www.almosafer.com/ar" };
+
+		int RandomIndex = rand.nextInt(URLS.length);
+
+		driver.get(URLS[RandomIndex]);
+
+	}
+
+	@Test(priority = 8)
+	public void SwitchHotels() {
+
+		WebElement HotelTab = driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
+		HotelTab.click();
+
+		WebElement searchLoaction = driver.findElement(By.cssSelector(".sc-phbroq-2.uQFRS.AutoComplete__Input "));
+
+		if (driver.getCurrentUrl().contains("en")) {
+			String[] English = { "Dubai", "Jeddah", "Riyadh" };
+			int randomIndex = rand.nextInt(English.length);
+			searchLoaction.sendKeys(English[randomIndex]);
 		} else {
-			WebElement arabicOption = driver.findElement(By.cssSelector(".sc-gkFcWv.jJNggu"));
-			arabicOption.click();
-		}
-	}
-		@Test
-		public void SwitchHotels() {
-	
-			WebElement HotelTab=driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
-			HotelTab.click();
-			
-			WebElement searchLoaction = driver.findElement(By.cssSelector(".sc-phbroq-2.uQFRS.AutoComplete__Input "));
-			
-			if(driver.getCurrentUrl().contains("en")) {
-				String [] English = { "Dubai" , "Jeddah" , "Riyadh"};
-				int randomIndex = rand.nextInt(English.length);
-				searchLoaction.sendKeys(English[randomIndex]);
-			}else {
-					
-				if(driver.getCurrentUrl().contains("ar")) {
-					String[] Arabic = { "دبي" , "الرياض"};
-					int randomIndex = rand.nextInt(Arabic.length);
-					searchLoaction.sendKeys(Arabic[randomIndex]);
-					
-				}
-				
-				
+
+			if (driver.getCurrentUrl().contains("ar")) {
+				String[] Arabic = { "دبي", "الرياض" };
+				int randomIndex = rand.nextInt(Arabic.length);
+				searchLoaction.sendKeys(Arabic[randomIndex]);
+
 			}
-			
-			
+
 		}
-			
+
 	}
 
-
+}
